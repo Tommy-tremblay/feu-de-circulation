8e3a90bee654e84bc2e9c5785067e6f078c03f98
Tommy-tremblay <116100321+Tommy-tremblay@users.noreply.github.com>
2022-11-16 14:41:58 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 function jaune () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 }
 input.onButtonPressed(Button.A, function () {
-	
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    basic.pause(200)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    basic.pause(200)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    basic.pause(200)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    basic.pause(200)
+    pins.digitalWritePin(DigitalPin.P8, 1)
 })
 function blanc () {
     pins.digitalWritePin(DigitalPin.P16, 1)
@@ -11,22 +25,22 @@ function orange () {
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
 function rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
 }
 function vert () {
     pins.digitalWritePin(DigitalPin.P0, 1)
-}
-loops.everyInterval(500, function () {
-    vert()
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    basic.pause(5000)
-    jaune()
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(1000)
-    rouge()
     pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(5000)
-})
+    pins.digitalWritePin(DigitalPin.P2, 0)
+}
+pins.digitalWritePin(DigitalPin.P16, 0)
+pins.digitalWritePin(DigitalPin.P8, 1)
 basic.forever(function () {
     vert()
+    basic.pause(1000)
+    jaune()
+    basic.pause(500)
+    rouge()
+    basic.pause(1000)
 })
